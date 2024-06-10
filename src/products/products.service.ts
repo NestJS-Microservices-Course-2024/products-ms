b97bbbc03c90523c973c76371f2b9344e773ca79
Leiner Barrios <lj.barrios@uniandes.edu.co>
2024-06-09 20:37:03 -0500

    Fix product availability check and update error handling

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,9 @@
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common/dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -52,12 +48,15 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     const product = await this.product.findUnique({
       where: {
         id,
-        available: false,
+        available: true,
       },
     });
 
     if (!product) {
-      throw new NotFoundException('Product not found');
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: `Product with id ${id} not found`,
+      });
     }
 
     return product;
